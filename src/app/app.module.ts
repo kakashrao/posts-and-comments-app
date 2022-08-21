255ab3cd84ea4715cdd999ba8a82783b311438f2
Akash <kakashrao.2021@gmail.com>
2022-08-21 21:46:04 +0530

    posts-list and post-create component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
