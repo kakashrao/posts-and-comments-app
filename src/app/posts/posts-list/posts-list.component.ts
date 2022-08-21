7f9269daca94f1340e95d2411a922f2b73bc07b6
Akash <kakashrao.2021@gmail.com>
2022-08-21 23:22:57 +0530

    posts list ui created

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.scss']
 })
 export class PostsListComponent {
 
