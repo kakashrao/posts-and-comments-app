b24d5f0b8927ff7cb38dbe5962a75b9640a945f6
Akash <kakashrao.2021@gmail.com>
2022-08-21 22:33:13 +0530

    landing component created and header component ui created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full'},
+  { path: 'posts', component: PostsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingComponent } from './landing-console/landing.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingComponent,
+    HeaderComponent,
     PostsListComponent,
-    PostCreateComponent
+    PostCreateComponent,
   ],
   imports: [
     BrowserModule,
