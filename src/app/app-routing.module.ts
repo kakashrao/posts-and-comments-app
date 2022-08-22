79688e6b81f4b1da0738a78f0195dc386285419b
Akash <kakashrao.2021@gmail.com>
2022-08-22 23:26:09 +0530

    routing added for create post component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/posts', pathMatch: 'full'},
-  { path: 'posts', component: PostsListComponent }
+  { path: 'posts', component: PostsListComponent },
+  { path: 'create-post', component: PostCreateComponent}
 ];
 
 @NgModule({
--- src/app/landing-console/landing.component.ts
+++ src/app/landing-console/landing.component.ts
@@ -1,11 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
+  constructor(private _router: Router) {}
 
+  currentUrl: string = '';
+
+  ngOnInit(): void {
+    this._router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const length = event.url.split('/')['length'];
+        this.currentUrl = event.url.split('/')[length - 1];
+        // console.log(this.currentUrl);
+      }
+    });
+  }
 }
